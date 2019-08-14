144a3dd222f2e0d3bc0434473dce069a6c4873fa
Anastasia Pankina <anastaciap@ymail.com>
2019-08-14 18:50:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 function theBeatlesPlay(arrayOfMusicians,arrayOfInstruments){
   var array=[];
   for(let i=0;i<arrayOfMusicians.length; i++){
-    
+    array.push(arrayOfMusicians[i]+" "+arrayOfInstruments[i]);
   }
+  function 
 }
